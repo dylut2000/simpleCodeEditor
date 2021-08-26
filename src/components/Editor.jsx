825d23b012299d0d87b999b79fad319db4f59afd
Hardy Lutula <hardylutula@gmail.com>
2021-08-26 14:46:05 +0200

    editor + style added

---
+++ src/components/Editor.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/css/css";
+import { Controlled as ControlledEditor } from "react-codemirror2";
+
+const Editor = (props) => {
+  const { language, displayName, value, onChange } = props;
+
+  function handleChange(editor, data, value) {
+    onChange(value);
+  }
+
+  return (
+    <div className="editor-container">
+      <div className="editor-title">
+        {displayName}
+        <button>O/C</button>
+      </div>
+
+      <ControlledEditor
+        onBeforeChange={handleChange}
+        value={value}
+        className="code-mirror-wrapper"
+        options={{
+          lineWrapping: true,
+          lint: true,
+          mode: language,
+          theme: "material",
+          lineNumbers: true,
+        }}
+      />
+    </div>
+  );
+};
+
+export default Editor;
